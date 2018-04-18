2a3cc944b6511e82d3bd3c9cae5cff72c02aa363
marcrossi <marc.a.rossi12@gmail.com>
2018-04-17 23:36:14 -0400

    stuff

--- buffon/src/buffon.cpp
+++ buffon/src/buffon.cpp
@@ -1,30 +1,55 @@
+#include <algorithm>
 #include <iostream>
-#include <sstream>
 #include <ros/ros.h>
 #include <std_msgs/String.h>
+#include <sstream>
+#include <math.h>
 
+#include <eigen3/Eigen/Dense>
+#include <geometry_msgs/Point32.h>
+#include <geometry_msgs/Point.h>
+#include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/PointCloud.h>
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+#include <visualization_msgs/Marker.h>
 
-// Include any additional header or service files
-
+using Eigen::Matrix3f;
+using Eigen::MatrixXf;
+using Eigen::MatrixXd;
+using Eigen::Vector3f;
+using Eigen::Vector2f;
+using geometry_msgs::Point32;
+using geometry_msgs::Point;
+using geometry_msgs::Twist;
+using nav_msgs::Odometry;
+using visualization_msgs::Marker;
+using sensor_msgs::LaserScan;
+using sensor_msgs::PointCloud;
 using std::cout;
-using std::string;
+using std::vector;
+using namespace std;
 
-// Declare class variables and publishers
-std_msgs::String string_to_publish_;
+// Global Parameters
+const float gRobotRadius = 0.18;
+const float gRobotHeight = 0.36;
+const float gEpsilon = 0.15;
+const float gAMaxLin = 1 * 0.5; // m/s^2
+const float gAMaxRot = 1 * 2.0; // rad/s^2
+const float gVMaxLin = 0.5; // m/s
+const float gVMaxRot = 1.5; // rad/s
+const float gDT = 0.02; // s
 
-ros::Publisher string_publisher_;
+sensor_msgs::PointCloud scan; 
 
-// Define service and callback functions 
-
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "assignment1");
+int main(int argc, char **argv) 
+{
+  ros::init(argc, argv, "BuffonBot");
   ros::NodeHandle n;
+  ros::Rate loop(10); // 10 Hertz
 
-  // Perform operations defined in Assignment 1
-
-  ros::Rate loop(10);
-
-  while (ros::ok()) {
+  while (ros::ok()) 
+  {
     ros::spinOnce();
     loop.sleep();
   }
